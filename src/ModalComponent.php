fb398a448482ee1def93e337d584e9b73f2ffeec
Tom Ribeiro <34167065+HeyItsRio@users.noreply.github.com>
2023-05-04 11:13:43 +0200

    css bug fix

--- src/ModalComponent.php
+++ src/ModalComponent.php
@@ -17,14 +17,14 @@ abstract class ModalComponent extends Component implements Contract
     protected static array $maxWidths = [
         'sm' => 'sm:max-w-sm',
         'md' => 'sm:max-w-md',
-        'lg' => 'sm:max-w-md md:max-w-lg',
-        'xl' => 'sm:max-w-md md:max-w-xl',
-        '2xl' => 'sm:max-w-md md:max-w-xl lg:max-w-2xl',
-        '3xl' => 'sm:max-w-md md:max-w-xl lg:max-w-3xl',
-        '4xl' => 'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-4xl',
-        '5xl' => 'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-5xl',
-        '6xl' => 'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-5xl 2xl:max-w-6xl',
-        '7xl' => 'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-5xl 2xl:max-w-7xl',
+        'lg' => 'md:max-w-lg',
+        'xl' => 'md:max-w-xl',
+        '2xl' => 'md:max-w-xl lg:max-w-2xl',
+        '3xl' => 'md:max-w-xl lg:max-w-3xl',
+        '4xl' => 'md:max-w-xl lg:max-w-3xl xl:max-w-4xl',
+        '5xl' => 'md:max-w-xl lg:max-w-3xl xl:max-w-5xl',
+        '6xl' => 'md:max-w-xl lg:max-w-3xl xl:max-w-5xl 2xl:max-w-6xl',
+        '7xl' => 'md:max-w-xl lg:max-w-3xl xl:max-w-5xl 2xl:max-w-7xl',
     ];
 
     public function destroySkippedModals(): self
